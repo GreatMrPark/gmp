10a7ba37e83b7016d8cb8407eefb841941badb34
greatmrpark <greatmrpark@greatmrpark-DEV>
2020-12-16 09:55:39 +0900

    #0954 달력 작업중

---
+++ gmp-web/src/main/resources/static/devHelper/calendar/JsYearCalendar/locales/js-year-calendar.ko.js
@@ -0,0 +1,17 @@
+/**
+ * Korean translation for js-year-calendar
+ * Paul Great Mr Park
+ * Based on 
+ * Korean translation for bootstrap-datepicker
+ * greatmrpark@gmail.com
+ */
+ 
+Calendar.locales['ko'] = {
+	days: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+	daysShort: ["일요", "월요", "화요", "수요", "목요", "금요", "토요"],
+	daysMin: ["일", "월", "화", "수", "목", "금", "토"],
+	months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+	monthsShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+	weekShort: '주',
+	weekStart:0
+};
