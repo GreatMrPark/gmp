7333dbe8540e49a40abb22dfacfc841c6165955f
greatmrpark <greatmrpark@greatmrpark-DEV>
2021-02-04 10:08:12 +0900

    #1007 global

--- gmp-web/src/main/resources/static/devHelper/gmpChart/gmpChart.js
+++ gmp-web/src/main/resources/static/devHelper/gmpChart/gmpChart.js
@@ -30,8 +30,53 @@
     console.log("Gmpcharts");
     var _modules = {};
     function _registerModule(obj, path, args, fn) {
+//        console.log(obj);
+//        console.log(path);
+//        console.log(args);
+//        console.log(fn);
+//        console.log(obj.hasOwnProperty(path));
         if (!obj.hasOwnProperty(path)) {
             obj[path] = fn.apply(null, args);
+//            console.log(obj[path]);
         }
+        console.log(obj);
     }
+    _registerModule(_modules, 'core/Globals.js', [], function () {
+        var glob = (
+            typeof win !== 'undefined' ?
+                win :
+                typeof window !== 'undefined' ?
+                    window :
+                    {}), doc = glob.document, SVG_NS = 'http://www.w3.org/2000/svg', userAgent = (glob.navigator && glob.navigator.userAgent) || '', svg = (doc &&
+                doc.createElementNS &&
+                !!doc.createElementNS(SVG_NS, 'svg').createSVGRect), isMS = /(edge|msie|trident)/i.test(userAgent) && !glob.opera, isFirefox = userAgent.indexOf('Firefox') !== -1, isChrome = userAgent.indexOf('Chrome') !== -1, hasBidiBug = (isFirefox &&
+                parseInt(userAgent.split('Firefox/')[1], 10) < 4 // issue #38
+            );
+        var G = {
+                product: 'Gmpcharts',
+                version: '8.2.2',
+                deg2rad: Math.PI * 2 / 360,
+                doc: doc,
+                hasBidiBug: hasBidiBug,
+                hasTouch: !!glob.TouchEvent,
+                isMS: isMS,
+                isWebKit: userAgent.indexOf('AppleWebKit') !== -1,
+                isFirefox: isFirefox,
+                isChrome: isChrome,
+                isSafari: !isChrome && userAgent.indexOf('Safari') !== -1,
+                isTouchDevice: /(Mobile|Android|Windows Phone)/.test(userAgent),
+                SVG_NS: SVG_NS,
+                chartCount: 0,
+                seriesTypes: {},
+                symbolSizes: {},
+                svg: svg,
+                win: glob,
+                marginNames: ['plotTop', 'marginRight', 'marginBottom', 'plotLeft'],
+                noop: function () { },
+                charts: [],
+                dateFormats: {}
+            };
+
+        return G;
+    });
 }));
